447fb83381eb683d864d2b8ef811efebc83f432e
lewcol <148833753+lewcol@users.noreply.github.com>
2025-10-06 14:47:29 +0100

    Configure User Routes

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,8 @@ const { initializeApp, applicationDefault } = require('firebase-admin/app');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const routes = require('./routes');
-const authenticateToken = require('./middleware/authenticateToken');
-//const errorHandler = require('./middleware/errorHandler');
+const webUserRoutes = require('./routes/web/userRoutes');
+const appUserRoutes = require('./routes/app/userRoutes');
 
 dotenv.config();
 
@@ -15,9 +14,11 @@ initializeApp({
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', routes);
+app.use('/api/web', webUserRoutes);
+app.use('api/app', appUserRoutes);
 
 app.listen(port, () => console.log('Express app listening on ${port}\\'))
