623fb1d528134b3bb3098a8512d78698e2da5301
lewcol <148833753+lewcol@users.noreply.github.com>
2025-10-06 14:48:30 +0100

    Correct typo in index.js

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/web', webUserRoutes);
-app.use('api/app', appUserRoutes);
+app.use('/api/app', appUserRoutes);
 
 app.listen(port, () => console.log('Express app listening on ${port}\\'))
