ef81696ce52651db4c36401463f5d359ab89b958
lewcol <148833753+lewcol@users.noreply.github.com>
2025-12-17 12:59:51 +0000

    Create Google Sign Up Button

---
+++ web-front/web-front/src/utils/createAccountWithGoogle.js
@@ -0,0 +1,27 @@
+import { auth } from "../index.js";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+export async function createAccountWithGoogle() {
+  try {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    console.log("User created successfully:", user);
+    console.log("User ID:", user.uid);
+
+    console.log("Operation Type:", result.operationType);
+
+    return user; 
+  } catch (error) {
+
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error("Error creating user:", errorCode, errorMessage);
+
+    if (errorCode === 'auth/google-in-use') {
+      alert("This Google account is already in use!");
+    }
+    throw error;
+  }
+}
